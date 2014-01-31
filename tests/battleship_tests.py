62eb55a3338bbc7e1d70846dbfc8d25d8006fa5d
Aziz Harrington <azizharrington@gmail.com>
2014-01-30 21:51:26 -0600

    moved render_ship to battlefield class

--- tests/battleship_tests.py
+++ tests/battleship_tests.py
@@ -5,8 +5,12 @@ from main.battleship import Battlefield, Ship
 
 
 def test_battlefield():
-    pass
+    battlefield = Battlefield()
+
+    assert_equal(10, len(battlefield.grid))
 
 
 def test_ship():
-    pass
+    carrier = Ship('carrier', [8, 'E'], 'accross')
+
+    assert_equal(5, carrier.length)
